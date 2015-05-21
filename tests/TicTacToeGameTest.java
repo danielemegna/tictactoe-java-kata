8dfee3048373eeb532016f86897a529b73f1ca6d
Daniele Megna <megna.dany@gmail.com>
2015-05-21 08:30:50 +0200

    First commit, asserting nothing. Environment is running.

---
+++ tests/TicTacToeGameTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by daniele on 21/05/15.
+ */
+public class TicTacToeGameTest {
+
+    @Test
+    public void nothing()
+    {
+        assertTrue(true);
+    }
+}
