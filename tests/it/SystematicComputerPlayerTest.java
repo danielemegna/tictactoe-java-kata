48b093a081dc407204dd63d3320cb213d104754a
Daniele Megna <megna.dany@gmail.com>
2015-06-09 22:34:44 +0200

    Refactoring SCP tests

--- tests/it/SystematicComputerPlayerTest.java
+++ tests/it/SystematicComputerPlayerTest.java
@@ -3,33 +3,39 @@ package it;
 import TicTacToe.Cell.Matrix;
 import TicTacToe.ComputerPlayer.SystematicComputerPlayer;
 import TicTacToe.Coordinates.Coordinates;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class SystematicComputerPlayerTest {
 
+    private Matrix m;
+    private SystematicComputerPlayer cp;
+
+    @Before
+    public void setup() {
+        this.m = new Matrix();
+        this.cp = new SystematicComputerPlayer();
+    }
+
     @Test
     public void withEmptyMatrix_SystematicComputerPlayerWillChooseZeroZero() {
-        Matrix m = new Matrix();
-        SystematicComputerPlayer cp = new SystematicComputerPlayer();
-
         assertEquals(new Coordinates(0, 0), cp.establishTheNextMove(m));
     }
 
     @Test
     public void SystematicComputer_isAlwaysPredictable() {
-        Matrix m = new Matrix();
-        SystematicComputerPlayer cp = new SystematicComputerPlayer();
-
         m.playerMark(new Coordinates(0, 0));
-        assertEquals(new Coordinates(1, 0), cp.establishTheNextMove(m));
-        m.computerMark(new Coordinates(1, 0));
+        assertNextMoveAndMarkIt(new Coordinates(1, 0));
         m.playerMark(new Coordinates(2, 0));
-        assertEquals(new Coordinates(0, 1), cp.establishTheNextMove(m));
-        m.computerMark(new Coordinates(0, 1));
+        assertNextMoveAndMarkIt(new Coordinates(0, 1));
         m.playerMark(new Coordinates(2, 1));
-        assertEquals(new Coordinates(1, 1), cp.establishTheNextMove(m));
-        m.computerMark(new Coordinates(1, 1));
+        assertNextMoveAndMarkIt(new Coordinates(1, 1));
+    }
+
+    private void assertNextMoveAndMarkIt(Coordinates expected) {
+        assertEquals(expected, cp.establishTheNextMove(m));
+        m.computerMark(expected);
     }
 }
