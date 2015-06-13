fdbbcbb8ed5e5a0a5a0b8a2c90e3b8f2b6abac6c
Daniele Megna <megna.dany@gmail.com>
2015-06-13 17:30:39 +0200

    Refactoring TerminalDisplay tests

--- tests/it/TerminalDisplayTest.java
+++ tests/it/TerminalDisplayTest.java
@@ -4,39 +4,52 @@ import TicTacToe.Cell.Matrix;
 import TicTacToe.Coordinates.Coordinates;
 import TicTacToe.Display.Display;
 import TicTacToe.Display.TerminalDisplay;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class TerminalDisplayTest {
 
-    @Test
-    public void displayAnEmptyMatrix() {
-        Display d = new TerminalDisplay();
-        String expected = "- - -\n- - -\n- - -";
-        assertEquals(expected, d.show(new Matrix()));
+    private Display display;
+    private Matrix matrix;
+
+    @Before
+    public void setup() {
+        display = new TerminalDisplay();
+        matrix = new Matrix();
     }
 
     @Test
-    public void displayAPlayerMarkAsX() {
-        Display d = new TerminalDisplay();
-        String expected = "- X -\n- - -\n- - -";
-        Matrix m = new Matrix();
-        m.playerMark(new Coordinates(1, 0));
-        assertEquals(expected, d.show(m));
+    public void showAnEmptyMatrix() {
+        String expected = "- - -\n" +
+                          "- - -\n" +
+                          "- - -";
+
+        assertEquals(expected, display.show(matrix));
     }
 
     @Test
-    public void displaySomePlayerMarks() {
-        Display d = new TerminalDisplay();
-        String expected = "- X -\nX - -\n- X X";
+    public void showAPlayerMarkAsX() {
+        String expected = "- X -\n" +
+                          "- - -\n" +
+                          "- - -";
 
-        Matrix m = new Matrix();
-        m.playerMark(new Coordinates(1, 0));
-        m.playerMark(new Coordinates(0, 1));
-        m.playerMark(new Coordinates(1, 2));
-        m.playerMark(new Coordinates(2, 2));
+        matrix.playerMark(new Coordinates(1, 0));
+        assertEquals(expected, display.show(matrix));
+    }
 
-        assertEquals(expected, d.show(m));
+    @Test
+    public void showSomePlayerMarks() {
+        String expected = "- X -\n" +
+                          "X - -\n" +
+                          "- X X";
+
+        matrix.playerMark(new Coordinates(1, 0));
+        matrix.playerMark(new Coordinates(0, 1));
+        matrix.playerMark(new Coordinates(1, 2));
+        matrix.playerMark(new Coordinates(2, 2));
+
+        assertEquals(expected, display.show(matrix));
     }
 }
