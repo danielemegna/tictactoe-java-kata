448c457694f8e389f65d3da504e2c0ac32129123
Daniele Megna <megna.dany@gmail.com>
2015-06-14 16:30:44 +0200

    Refactoring UnbeatableComputerPlayer tests

--- tests/it/UnbeatableComputerPlayerTest.java
+++ tests/it/UnbeatableComputerPlayerTest.java
@@ -4,28 +4,36 @@ import TicTacToe.Cell.Matrix;
 import TicTacToe.ComputerPlayer.ComputerPlayer;
 import TicTacToe.ComputerPlayer.UnbeatableComputerPlayer;
 import TicTacToe.Coordinates.Coordinates;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class UnbeatableComputerPlayerTest {
 
+    private ComputerPlayer cp;
+    private Matrix matrix;
+
+    @Before
+    public void setup() {
+        cp = new UnbeatableComputerPlayer();
+        matrix = new Matrix();
+    }
+
     @Test
     public void cpAlwaysChoosesZeroZeroAsFirstMove() {
-        ComputerPlayer cp = new UnbeatableComputerPlayer();
-        Matrix matrix = new Matrix();
-
-        Coordinates c = cp.establishTheNextMove(matrix);
-        assertEquals(new Coordinates(0, 0), c);
+        assertNextMove(0, 0);
     }
 
     @Test
     public void choosesOneOneAsNotFirstMoveIfAvailable() {
-        ComputerPlayer cp = new UnbeatableComputerPlayer();
-        Matrix matrix = new Matrix();
         matrix.playerMark(new Coordinates(0, 0));
+        assertNextMove(1, 1);
+    }
 
-        Coordinates c = cp.establishTheNextMove(matrix);
-        assertEquals(new Coordinates(1, 1), c);
+    private void assertNextMove(int x, int y) {
+        Coordinates expected = new Coordinates(x, y);
+        Coordinates actual = cp.establishTheNextMove(matrix);
+        assertEquals(expected, actual);
     }
 }
