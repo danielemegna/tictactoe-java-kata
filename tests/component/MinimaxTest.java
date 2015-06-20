fc934f5f9bc13a85ed2bb48130874aa057ae83d3
Daniele Megna <megna.dany@gmail.com>
2015-06-20 21:37:18 +0200

    Refactoring Minimax tests

--- tests/component/MinimaxTest.java
+++ tests/component/MinimaxTest.java
@@ -3,25 +3,32 @@ package component;
 import TicTacToe.Cell.Matrix;
 import TicTacToe.ComputerPlayer.Minimax;
 import TicTacToe.Coordinates.Coordinates;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class MinimaxTest {
 
+    private Minimax minimax;
+    private Matrix matrix;
+
+    @Before
+    public void setup() {
+        this.minimax = new Minimax();
+        this.matrix = new Matrix();
+    }
+
     @Test
     public void lastTieMove_worthZero() {
-        Minimax minimax = new Minimax();
-        Matrix matrix = new Matrix();
-
-          matrix.playerMark(new Coordinates(0, 0));
-        matrix.computerMark(new Coordinates(1, 0));
-        matrix.computerMark(new Coordinates(2, 0));
-        matrix.computerMark(new Coordinates(0, 1));
-          matrix.playerMark(new Coordinates(2, 1));
-          matrix.playerMark(new Coordinates(0, 2));
-          matrix.playerMark(new Coordinates(1, 2));
-        matrix.computerMark(new Coordinates(2, 2));
+          playerMark(0, 0);
+        computerMark(1, 0);
+        computerMark(2, 0);
+        computerMark(0, 1);
+          playerMark(2, 1);
+          playerMark(0, 2);
+          playerMark(1, 2);
+        computerMark(2, 2);
 
         int actual = minimax.calcolateMoveValue(new Coordinates(1, 1), matrix);
         assertEquals(0, actual);
@@ -29,22 +36,27 @@ public class MinimaxTest {
 
     @Test
     public void winnerWithAMoveAndTwoPossibilities() {
-        Minimax minimax = new Minimax();
-        Matrix matrix = new Matrix();
-
-          matrix.playerMark(new Coordinates(0, 0));
-        matrix.computerMark(new Coordinates(1, 0));
-          matrix.playerMark(new Coordinates(2, 0));
-          matrix.playerMark(new Coordinates(0, 1));
-        matrix.computerMark(new Coordinates(1, 1));
-          matrix.playerMark(new Coordinates(2, 1));
-        matrix.computerMark(new Coordinates(0, 2));
+          playerMark(0, 0);
+        computerMark(1, 0);
+          playerMark(2, 0);
+          playerMark(0, 1);
+        computerMark(1, 1);
+          playerMark(2, 1);
+        computerMark(0, 2);
 
         int winnerMoveValue = minimax.calcolateMoveValue(new Coordinates(1, 2), matrix);
         assertEquals(1, winnerMoveValue);
 
         int tieMoveValue = minimax.calcolateMoveValue(new Coordinates(2, 2), matrix);
         assertEquals(0, tieMoveValue);
+    }
+
+    private void playerMark(int x, int y) {
+        matrix.playerMark(new Coordinates(x, y));
+    }
+
+    private void computerMark(int x, int y) {
+        matrix.computerMark(new Coordinates(x, y));
     }
 
 }
