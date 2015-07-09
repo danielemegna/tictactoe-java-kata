1eff29db640c7e280de5e3913b4a433ad2e389c1
Daniele Megna <megna.dany@gmail.com>
2015-07-09 08:05:21 +0200

    Board class review

--- src/TicTacToe/Cell/Board.java
+++ src/TicTacToe/Cell/Board.java
@@ -14,6 +14,7 @@ public class Board {
 
     public Board() {
         this.cells = new Cell[BOARD_DIMENSION][BOARD_DIMENSION];
+
         for(int x = 0; x < BOARD_DIMENSION; x++)
             for(int y = 0; y < BOARD_DIMENSION; y++)
                 this.cells[x][y] = new Cell();
@@ -41,11 +42,12 @@ public class Board {
 
     public Set<Coordinates> getEmptyCoordinates() {
         Set<Coordinates> emptyCoordinates = new HashSet<>();
+
         for(int x = 0; x < BOARD_DIMENSION; x++) {
             for (int y = 0; y < BOARD_DIMENSION; y++) {
                 Coordinates c = new Coordinates(x, y);
                 if (getCellState(c) == CellState.Empty)
-                    emptyCoordinates.add(new Coordinates(x, y));
+                    emptyCoordinates.add(c);
             }
         }
 
@@ -54,6 +56,7 @@ public class Board {
 
     public Board clone() {
         Board clone = new Board();
+
         for(int x = 0; x < BOARD_DIMENSION; x++) {
             for (int y = 0; y < BOARD_DIMENSION; y++) {
                 Coordinates c = new Coordinates(x, y);
