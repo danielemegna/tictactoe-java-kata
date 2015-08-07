cc544b972268d02f18da30c0ccb9e454b4d3b1b0
Daniele Megna <megna.dany@gmail.com>
2015-08-07 13:04:40 +0200

    Simplified and splitted Coordinates test cases

--- tests/unit/CoordinatesTest.java
+++ tests/unit/CoordinatesTest.java
@@ -10,46 +10,35 @@ public class CoordinatesTest {
     @Test
     public void trivialInstantiateAndGetCoordinates()
     {
-        Coordinates c;
-
-        c= new Coordinates(0, 1);
+        Coordinates c = new Coordinates(0, 1);
         assertEquals(0, c.getX());
         assertEquals(1, c.getY());
-
-        c= new Coordinates(2, 0);
-        assertEquals(2, c.getX());
-        assertEquals(0, c.getY());
     }
 
 
-    @Test
-    public void createCoordinatesWithWrongParams_throwsACoordinateOutOfBoundsException()
-    {
-        String failMessage = "Expected CoordinateOutOfBoundsException not catched";
-
-        try { new Coordinates(0, 3); fail(failMessage); }
-        catch(CoordinateOutOfBoundsException ex) {}
-        try { new Coordinates(5, 1); fail(failMessage); }
-        catch(CoordinateOutOfBoundsException ex) {}
-        try { new Coordinates(42, 13); fail(failMessage); }
-        catch(CoordinateOutOfBoundsException ex) {}
-        try { new Coordinates(1, -2); fail(failMessage); }
-        catch(CoordinateOutOfBoundsException ex) {}
+    @Test(expected = CoordinateOutOfBoundsException.class)
+    public void createCoordinatesWithWrongParams_throwsAnException() {
+        new Coordinates(5, 1);
     }
 
     @Test
-    public void coordinatesAreValueObjects () {
-        Coordinates zeroZero = new Coordinates(0, 0);
-        Coordinates anotherZeroZero = new Coordinates(0, 0);
-        Coordinates differentCoordinates = new Coordinates(1, 0);
+    public void coordinatesEquality() {
+        Coordinates zeroZero                = new Coordinates(0, 0);
+        Coordinates anotherZeroZero         = new Coordinates(0, 0);
+        Coordinates differentCoordinates    = new Coordinates(1, 0);
 
         assertEquals(zeroZero, anotherZeroZero);
-        assertEquals(zeroZero.hashCode(), anotherZeroZero.hashCode());
-
         assertNotEquals(zeroZero, differentCoordinates);
-        assertNotEquals(anotherZeroZero, differentCoordinates);
+    }
+
+    @Test
+    public void coordinatesHashCodesEquality() {
+        Coordinates zeroZero                = new Coordinates(0, 0);
+        Coordinates anotherZeroZero         = new Coordinates(0, 0);
+        Coordinates differentCoordinates    = new Coordinates(1, 0);
+
+        assertEquals(zeroZero.hashCode(), anotherZeroZero.hashCode());
         assertNotEquals(zeroZero.hashCode(), differentCoordinates.hashCode());
-        assertNotEquals(anotherZeroZero.hashCode(), differentCoordinates.hashCode());
     }
 
 }
