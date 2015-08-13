a181f4d258f335764b5f9160b874ab5fc8173e96
Daniele Megna <megna.dany@gmail.com>
2015-08-13 11:36:50 +0200

    Testing display messages

--- tests/unit/ConsoleDisplayTest.java
+++ tests/unit/ConsoleDisplayTest.java
@@ -1,8 +1,10 @@
 package unit;
 
+import TicTacToe.Cell.CellMarkSign;
 import TicTacToe.Display.ConsoleDisplay;
 import TicTacToe.Game.GameMode;
 import helpers.SpyIOBridge;
+import helpers.SpyPlayer;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -53,6 +55,31 @@ public class ConsoleDisplayTest {
             "println(3. Computer vs Human)",
             "println(4. Computer vs Computer)",
             "readLine(->)"
+        ), spyIOBridge.calls());
+    }
+
+    @Test
+    public void askForHumanPlayerName() {
+        display.askForHumanPlayerName();
+
+        assertEquals(Arrays.asList(
+            "readLine(Human player name?)"
+        ), spyIOBridge.calls());
+    }
+
+    @Test
+    public void announcingWinner() {
+        display.announceWinner(new SpyPlayer(CellMarkSign.Cross, "BobTheSpy"));
+        assertEquals(Arrays.asList(
+            "println(BobTheSpy is the winner!)"
+        ), spyIOBridge.calls());
+    }
+
+    @Test
+    public void announcingTie() {
+        display.announceTie();
+        assertEquals(Arrays.asList(
+            "println(Tie game, no winner.)"
         ), spyIOBridge.calls());
     }
 }
