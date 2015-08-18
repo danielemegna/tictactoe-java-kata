13aba220691f702b21fc86ed2090cdf9c2a8b3d4
Daniele Megna <megna.dany@gmail.com>
2015-08-18 10:31:55 +0200

    Removed useless exception declarations

--- src/TicTacToe/Main.java
+++ src/TicTacToe/Main.java
@@ -3,11 +3,9 @@ package TicTacToe;
 import TicTacToe.Game.Game;
 import TicTacToe.Display.ConsoleDisplay;
 
-import java.io.IOException;
-
 public class Main {
 
-    public static void main(String args[]) throws IOException, InterruptedException {
+    public static void main(String args[]) {
         IOBridge ioBridge           = new IOBridge(System.in, System.out);
         ConsoleDisplay display      = new ConsoleDisplay(ioBridge);
         Game game                   = new Game(display);
